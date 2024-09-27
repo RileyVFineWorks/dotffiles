57037d6f37438c5db3230d13f417714c0738818c
Riley Velasco <99685899+RileyVFineWorks@users.noreply.github.com>
2024-09-27 14:27:19 -0700

    Update remap.lua

--- nvim/lua/riley/remap.lua
+++ nvim/lua/riley/remap.lua
@@ -1,2 +1,4 @@
 vim.keymap.set('n','<leader>r', vim.cmd.Ex)
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeOpen<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>ToggleTerm<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<C-t>', '<cmd>ToggleTerm<CR>', {noremap = true, silent = true})
